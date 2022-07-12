375b2f8db1285f460bf256fcebf58458ac9efb94
alvannwanorim <alvannwanorim@gmail.com>
2022-07-12 14:34:02 +0100

    new changes

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,14 +1,14 @@
 export const config = {
-  'username': process.env.POSTGRES_USERNAME,
-  'password': process.env.POSTGRES_PASSWORD,
-  'database': process.env.POSTGRES_DB,
-  'host': process.env.POSTGRES_HOST,
-  'dialect': 'postgres',
-  'aws_region': process.env.AWS_REGION,
-  'aws_profile': process.env.AWS_PROFILE,
-  'aws_media_bucket': process.env.AWS_BUCKET,
-  'url': process.env.URL,
-  'jwt': {
-    'secret': process.env.JWT_SECRET,
-  },
+	username: 'postgres',
+	password: 'alvan2327',
+	database: 'alvandb',
+	host: 'alvandb.c56vg5ee7j5h.us-east-2.rds.amazonaws.com',
+	dialect: 'postgres',
+	aws_region: 'us-east-2',
+	aws_profile: 'default',
+	aws_media_bucket: 'alvan1970339bucket',
+	url: 'http://localhost:8100',
+	jwt: {
+		secret: 'testing'
+	}
 };
